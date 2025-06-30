53005f7c11494f24baff15088755c9e44548cc66
Santiago Rodriguez <santiago.rodriguez@intive.com>
2025-06-30 16:16:52 -0500

    getting started the course, day 1 is now completed.

---
+++ task.py
@@ -0,0 +1,5 @@
+# This line fo code will take an input using the input() function
+name = input("What is your name? ")
+size = len(name)
+print(f"Hello, {name}!")
+print(f"Your name has {size} characters.")
